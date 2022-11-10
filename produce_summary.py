4648b46c1ca61481d6193cb5408573b05d872034
Sandrita <sandramilenagarciaa@gmail.com>
2022-11-09 19:44:51 -0800

    initial commit

--- produce_summary.py
+++ produce_summary.py
@@ -1,40 +1,58 @@
-print("Day 1")
-the_file = open("um-deliveries-day-1.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+def summary_report(day):
+    while 1:
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+        user_input = input('what day do you want to look up? ')
 
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+        if user_input == "Day1":
+            print("Day 1")
+            the_file = open("um-deliveries-day-1.txt")
+        elif user_input == "Day2":
+            print("Day 2")
+            the_file = open("um-deliveries-day-2.txt")
+        elif user_input == "Day3":
+            print("Day 3")
+            the_file = open("um-deliveries-day-3.txt")
+
+        for line in the_file:
+            line = line.rstrip()
+            words = line.split(' ')
+            melon = words[0]
+            count = words[0]
+            amount = words[0]
+
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+        the_file.close()
+
+    return (day)
 
 
-print("Day 2")
-the_file = open("um-deliveries-day-2.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+day = "Day1"
+bucket = summary_report(day)
 
 
-print("Day 3")
-the_file = open("um-deliveries-day-3.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+# print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+# print("Day 2")
+# the_file = open("um-deliveries-day-2.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
 
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
+
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
+
+
+# print("Day 3")
+# the_file = open("um-deliveries-day-3.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
